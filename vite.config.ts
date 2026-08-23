e6b262af82c677272aa044c7f469820a51e36bd4
imenload <greenfeeda@gmail.com>
2026-08-23 10:20:41 +0300

    chore: increase workbox maximumFileSizeToCacheInBytes to 10MB to accommodate expanded secrets array

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,9 @@ export default defineConfig(() => {
               type: 'image/svg+xml'
             }
           ]
+        },
+        workbox: {
+          maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MB limit
         }
       })
     ],
